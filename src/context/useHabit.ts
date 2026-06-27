08164b5624123b6889a8c2958cbefe83f096902c
asiagorczyca <jgorczyca@student.agh.edu.pl>
2026-06-27 11:28:48 +0200

    first commit

---
+++ src/context/useHabit.ts
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+export type Habit = {
+  id: string;
+  name: string;
+  completions: Date[];
+};
+
+
+type Context = {
+  habits: Habit[];
+  addHabit: (name: string) => void;
+  deleteHabit: (id: string) => void;
+  toggleHabit: (id: string, date: Date) => void;
+};
+
+export const HabitContext = createContext<null | Context>(null);
+
+export function useHabit() {
+    const habitContext = useContext(HabitContext)
+    if (habitContext == null) throw new Error("Null context")
+        
+    return habitContext
+}
